c251297a6c24bec37000776b4c27bb397d0630d1
tcruz88 <tcruz88@gmail.com>
2021-03-05 15:21:05 -0500

    intial commit, django and python ML models

---
+++ Titanic/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from . import ml_predict
+def home(request):
+    return render(request, "index.html")
+
+def result(request):
+    pclass = request.GET["pclass"]
+    sex = request.GET["sex"]
+    age = request.GET["age"]
+    sibsp = request.GET["sibsp"]
+    parch = request.GET["parch"]
+    fare = request.GET["fare"]
+    embarked = request.GET["embarked"]
+    title = request.GET["title"]
+    prediction = ml_predict.prediction_model(pclass, sex, age, sibsp, parch, fare, embarked, title)
+    return render(request, "result.html", {'prediction':prediction})
